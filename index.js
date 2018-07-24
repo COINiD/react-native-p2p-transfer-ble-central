f482ef5e59c7647108fcab1ac124ae2974c31f21
rikardwissing <rikard@nocweb.se>
2018-07-24 02:12:44 +0200

    update

--- index.js
+++ index.js
@@ -6,217 +6,217 @@ const bleCentralModule = NativeModules.P2PTransferBLECentralModule;
 const bleCentralEmitter = new NativeEventEmitter(bleCentralModule);
 
 class BLECentral extends EventEmitter {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		bleCentralEmitter.addListener('didUpdateState', (data) => {
-			console.log('didUpdateState', data);
-		});
+    bleCentralEmitter.addListener('didUpdateState', (data) => {
+      console.log('didUpdateState', data);
+    });
 
-		bleCentralEmitter.addListener('didDiscoverPeripheral', (data) => {
-			console.log('didDiscoverPeripheral', data);
-		});
+    bleCentralEmitter.addListener('didDiscoverPeripheral', (data) => {
+      console.log('didDiscoverPeripheral', data);
+    });
 
-		bleCentralEmitter.addListener('didFailToConnectPeripheral', (data) => {
-			console.log('didFailToConnectPeripheral', data);
-		});
+    bleCentralEmitter.addListener('didFailToConnectPeripheral', (data) => {
+      console.log('didFailToConnectPeripheral', data);
+    });
 
-		bleCentralEmitter.addListener('didUpdateValueForCharacteristic', (data) => {
-			console.log('didUpdateValueForCharacteristic', data);
-		});
+    bleCentralEmitter.addListener('didUpdateValueForCharacteristic', (data) => {
+      console.log('didUpdateValueForCharacteristic', data);
+    });
 
-		bleCentralEmitter.addListener('didConnectPeripheral', (data) => {
-			console.log('didConnectPeripheral', data);
-		});
-		
-		bleCentralEmitter.addListener('didDisconnectPeripheral', (data) => {
-			console.log('didDisconnectPeripheral', data);
-		});
-	}
+    bleCentralEmitter.addListener('didConnectPeripheral', (data) => {
+      console.log('didConnectPeripheral', data);
+    });
+    
+    bleCentralEmitter.addListener('didDisconnectPeripheral', (data) => {
+      console.log('didDisconnectPeripheral', data);
+    });
+  }
 
-	disconnect = () => {
-		return new Promise((resolve, reject) => {
-				if(this.connectedPeripheralUUID === undefined) {
-					return resolve();
-				}
+  disconnect = () => {
+    return new Promise((resolve, reject) => {
+        if(this.connectedPeripheralUUID === undefined) {
+          return resolve();
+        }
 
-				this.unsubscribe().then(() => {
-					console.log("trying to disconnect");
-					bleCentralModule.disconnect(this.connectedPeripheralUUID, () => {
-						this.connectedPeripheralUUID = undefined;
-						return resolve();
-					});
-				});
-		});
-	}
+        this.unsubscribe().then(() => {
+          console.log("trying to disconnect");
+          bleCentralModule.disconnect(this.connectedPeripheralUUID, () => {
+            this.connectedPeripheralUUID = undefined;
+            return resolve();
+          });
+        });
+    });
+  }
 
-	unsubscribe = () => {
-		return new Promise((resolve, reject) => {
-			if(this.subscribedCharacteristic === undefined) {
-				return resolve();
-			}
+  unsubscribe = () => {
+    return new Promise((resolve, reject) => {
+      if(this.subscribedCharacteristic === undefined) {
+        return resolve();
+      }
 
-			const { serviceUUID, characteristicUUID } = this.subscribedCharacteristic;
+      const { serviceUUID, characteristicUUID } = this.subscribedCharacteristic;
 
-	  	bleCentralEmitter.removeAllListeners('transferStarted');
-	  	bleCentralEmitter.removeAllListeners('transferProgress');
-	  	bleCentralEmitter.removeAllListeners('transferDone');
+      bleCentralEmitter.removeAllListeners('transferStarted');
+      bleCentralEmitter.removeAllListeners('transferProgress');
+      bleCentralEmitter.removeAllListeners('transferDone');
 
-	  	console.log('trying to unsubscribed');
-	  	bleCentralModule.unSubscribeToCharacteristic(serviceUUID, characteristicUUID, () => {
-	  		console.log('unsubscribed...')
-	  		this.subscribedCharacteristic = undefined;
-	  		return resolve();
-	  	});
-	  });
+      console.log('trying to unsubscribed');
+      bleCentralModule.unSubscribeToCharacteristic(serviceUUID, characteristicUUID, () => {
+        console.log('unsubscribed...')
+        this.subscribedCharacteristic = undefined;
+        return resolve();
+      });
+    });
     }
 
-	connect = (filter) => {
-		const {serviceUUID, localName} = filter;
+  connect = (filter) => {
+    const {serviceUUID, localName} = filter;
 
-		return new Promise((resolve, reject) => {
-			bleCentralModule.start(started => {
+    return new Promise((resolve, reject) => {
+      bleCentralModule.start(started => {
 
-				console.log("module started and powered on");
+        console.log("module started and powered on");
 
-				if(!started) {
-					return reject('Failed to start BLE module');
-				}
+        if(!started) {
+          return reject('Failed to start BLE module');
+        }
 
-				this.disconnect().then(() => {
-					console.log("disconnected...");
+        this.disconnect().then(() => {
+          console.log("disconnected...");
 
-				  bleCentralModule.scanForPeripheralsWithServices({ serviceUUID, localName }, ({peripheralUUID}) => {
-				  	console.log("found peripheral", peripheralUUID);
-				  	bleCentralModule.stopScan();
+          bleCentralModule.scanForPeripheralsWithServices({ serviceUUID, localName }, ({peripheralUUID}) => {
+            console.log("found peripheral", peripheralUUID);
+            bleCentralModule.stopScan();
 
-				  	bleCentralModule.connect(peripheralUUID, connectedPeripheralUUID => {
-				  		if(peripheralUUID !== connectedPeripheralUUID) {
-				  			this.emit('connectionFailed');
-				  			return reject('Connection failed');
-				  		}
+            bleCentralModule.connect(peripheralUUID, connectedPeripheralUUID => {
+              if(peripheralUUID !== connectedPeripheralUUID) {
+                this.emit('connectionFailed');
+                return reject('Connection failed');
+              }
 
-				  		this.connectedPeripheralUUID = connectedPeripheralUUID;
-				  		
-				  		this.emit('connected', connectedPeripheralUUID);
-				  		return resolve(connectedPeripheralUUID);
-				  	});
-				  });
+              this.connectedPeripheralUUID = connectedPeripheralUUID;
+              
+              this.emit('connected', connectedPeripheralUUID);
+              return resolve(connectedPeripheralUUID);
+            });
+          });
 
-				});
-			});
-		});
-	}
+        });
+      });
+    });
+  }
 
-	discover = (serviceUUID, characteristicUUID) => {
-		return new Promise((resolve, reject) => {
-			bleCentralModule.discoverServices(serviceUUID, data => {
-				console.log("discovered serv...", data);
-		        
-				bleCentralModule.discoverCharacteristics(serviceUUID, characteristicUUID, (data) => {
-		      console.log("discover char...", data);
-		      return resolve(data);
-		    });
-			});
-		});
-	}
+  discover = (serviceUUID, characteristicUUID) => {
+    return new Promise((resolve, reject) => {
+      bleCentralModule.discoverServices(serviceUUID, data => {
+        console.log("discovered serv...", data);
+            
+        bleCentralModule.discoverCharacteristics(serviceUUID, characteristicUUID, (data) => {
+          console.log("discover char...", data);
+          return resolve(data);
+        });
+      });
+    });
+  }
 
-	connectAndDiscover = (filter) => {
-		const {serviceUUID, localName, characteristicUUID} = filter;
+  connectAndDiscover = (filter) => {
+    const {serviceUUID, localName, characteristicUUID} = filter;
 
-		return new Promise((resolve, reject) => {
-			this.connect({serviceUUID, localName})
-			.then(peripheralUUID => {
-				console.log("connected:", peripheralUUID);
+    return new Promise((resolve, reject) => {
+      this.connect({serviceUUID, localName})
+      .then(peripheralUUID => {
+        console.log("connected:", peripheralUUID);
 
-				this.discover(serviceUUID, characteristicUUID)
-				.then(data => {
-					return resolve(data);
-				});
-			});
-		});
-	}
+        this.discover(serviceUUID, characteristicUUID)
+        .then(data => {
+          return resolve(data);
+        });
+      });
+    });
+  }
 
-	sendData = (value, filter) => {
-		return new Promise((resolve, reject) => {
+  sendData = (value, filter) => {
+    return new Promise((resolve, reject) => {
       const {serviceUUID, localName} = filter;
       const characteristicUUID = '3333'; // Special characteristic for sending data. Peripheral listens to this.
 
-			if(!serviceUUID) {
-				return reject("serviceUUID required filter");
-			}
+      if(!serviceUUID) {
+        return reject("serviceUUID required filter");
+      }
 
       bleCentralModule.setSendCharacteristic(characteristicUUID);
 
-			this.connectAndDiscover({serviceUUID, localName, characteristicUUID})
-			.then(data => {
-				console.log("discovered serv...", data);
-				this.emit('sendingStarted', data);
+      this.connectAndDiscover({serviceUUID, localName, characteristicUUID})
+      .then(data => {
+        console.log("discovered serv...", data);
+        this.emit('sendingStarted', data);
 
-				bleCentralEmitter.addListener('didWriteValueForCharacteristic', (data) => {
-					this.emit('sendingProgress', data);
-				});
+        bleCentralEmitter.addListener('didWriteValueForCharacteristic', (data) => {
+          this.emit('sendingProgress', data);
+        });
 
         bleCentralModule.writeValueForCharacteristic(serviceUUID, characteristicUUID, value, () => {
-        	bleCentralEmitter.removeAllListeners('didWriteValueForCharacteristic');
+          bleCentralEmitter.removeAllListeners('didWriteValueForCharacteristic');
 
-        	this.emit('sendingDone', data);
+          this.emit('sendingDone', data);
           return resolve();
         });
-			});
-		});
-	}
+      });
+    });
+  }
 
-	receiveData = (filter) => {
-		return new Promise((resolve, reject) => {
+  receiveData = (filter) => {
+    return new Promise((resolve, reject) => {
       const {serviceUUID, localName} = filter;
       const characteristicUUID = '2222'; // Special characteristic for receiving data. Peripheral starts sending once Central subscribes.
 
-			if(!serviceUUID) {
-				return reject("serviceUUID required filter");
-			}
-			
+      if(!serviceUUID) {
+        return reject("serviceUUID required filter");
+      }
+      
       bleCentralModule.setReceiveCharacteristic(characteristicUUID);
 
-			this.connectAndDiscover({serviceUUID, localName, characteristicUUID})
-			.then(data => {
+      this.connectAndDiscover({serviceUUID, localName, characteristicUUID})
+      .then(data => {
 
         bleCentralModule.subscribeToCharacteristic(serviceUUID, characteristicUUID, (data) => {
-        	console.log("subscribed...", data);
-        	this.subscribedCharacteristic = { serviceUUID, characteristicUUID };
+          console.log("subscribed...", data);
+          this.subscribedCharacteristic = { serviceUUID, characteristicUUID };
 
-        	if(!data) {
-        		reject('Error when subscribing to characteristic');
-        		return ;
-        	}
+          if(!data) {
+            reject('Error when subscribing to characteristic');
+            return ;
+          }
 
-			    bleCentralEmitter.addListener('transferStarted', (data) => {
-			    	bleCentralEmitter.removeAllListeners('transferStarted');
-			    	this.emit('receivingStarted', data);
-			    });
+          bleCentralEmitter.addListener('transferStarted', (data) => {
+            bleCentralEmitter.removeAllListeners('transferStarted');
+            this.emit('receivingStarted', data);
+          });
 
-			    bleCentralEmitter.addListener('transferProgress', (data) => {
-			    	this.emit('receivingProgress', data);
-			    });
+          bleCentralEmitter.addListener('transferProgress', (data) => {
+            this.emit('receivingProgress', data);
+          });
 
-			    bleCentralEmitter.addListener('transferDone', (data) => {
-			    	this.unsubscribe().then(() => {
-			    		bleCentralModule.writeValueForCharacteristic(serviceUUID, characteristicUUID, 'finished', () => {
-			    			this.disconnect().then(() => {
-					    		this.emit('receivingDone', data);
-					    		return resolve(data);
-			    			});
-			    		});
-			    	})
-			    });
-			  });
+          bleCentralEmitter.addListener('transferDone', (data) => {
+            this.unsubscribe().then(() => {
+              bleCentralModule.writeValueForCharacteristic(serviceUUID, characteristicUUID, 'finished', () => {
+                this.disconnect().then(() => {
+                  this.emit('receivingDone', data);
+                  return resolve(data);
+                });
+              });
+            })
+          });
+        });
 
-			})
-			.catch(() => {
-				return reject('Error when connecting to characteristic');
-			});
-		});
-	}
+      })
+      .catch(() => {
+        return reject('Error when connecting to characteristic');
+      });
+    });
+  }
 }
 
 module.exports = new BLECentral();
